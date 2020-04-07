c9a4857685fde6880b12b8c9b08107e89310c0a3
timwuu <timwuu@gmail.com>
2020-04-07 18:08:11 +0800

    saved changes

--- Solver.py
+++ Solver.py
@@ -1,5 +1,8 @@
 import numpy as np
 import hashlib
+import copy
+
+# Notation: [ROW][COL]
 
 class STATE:
     
@@ -64,6 +67,24 @@ class STATE:
         m.update( self._box.tobytes())  
         return m.hexdigest()
     
+    # print( "Move Box:", box_no, "Steps:", steps, "Dir:", mov_dir)
+    def moveBox( self, box_no, mov_dir):
+
+        self._player[0] = self._box[box_no][0] 
+        self._player[1] = self._box[box_no][1]
+
+        self._box[box_no][0] += mov_dir[0] 
+        self._box[box_no][1] += mov_dir[1]
+    
+    def matchGoal( self, goal):
+
+        for elem in self._box:
+            if( [elem[0],elem[1]] not in goal):
+                return False
+
+        return True
+
+
 """ def CountSteps2( map, state):
 
     i=1
@@ -131,16 +152,16 @@ def CountSteps2( map, state):
             x = elem[0]
             y = elem[1]
 
-            if( map[x-1][y]==0):    #LEFT
+            if( map[x-1][y]==0):    #UP
                 map[x-1][y] = i
                 next_lst.append([x-1,y])
-            if( map[x+1][y]==0):    #RIGHT
+            if( map[x+1][y]==0):    #DOWN
                 map[x+1][y] = i
                 next_lst.append([x+1,y])
-            if( map[x][y-1]==0):    #UP
+            if( map[x][y-1]==0):    #LEFT
                 map[x][y-1] = i
                 next_lst.append([x,y-1])
-            if( map[x][y+1]==0):    #DOWN
+            if( map[x][y+1]==0):    #RIGHT
                 map[x][y+1] = i
                 next_lst.append([x,y+1])
 
@@ -152,34 +173,142 @@ def CountSteps2( map, state):
 
         pass
 
+    #set map[i][j]==0 to -9
+    for i in range(0,8):
+        for j in range(0,8):
+            if( map[i][j]==0):
+                map[i][j]= -9
+
+    map[state._player[0]][state._player[1]] = 0
+
     pass
 
+#  0: BLANK
+# -1: WALL
+# -2: BOX
+# -3: PLAYER
 def CountSteps( map, state):
 
     #print( state.get_hexdigest())
 
-    map2 = map.copy()
+    #map2 = map.copy()
 
+    # Add BOX, PLAYER to map
     for val in state._box:
-        map2[val[0]][val[1]]= -2
+        map[val[0]][val[1]]= -2
 
-    map2[state._player[0]][state._player[1]] = -3
+    map[state._player[0]][state._player[1]] = -3
 
-    print( map2)
-    CountSteps2( map2, state)
-
-    print( map2)
+    #print( map)
+    CountSteps2( map, state)
 
     pass
 
+def SetEligibleMoves( map, state, moves):
+    
+    i=0
+
+    for elem in state._box:
+        x = elem[0]
+        y = elem[1]
+
+        if( map[x-1][y]>=0 and map[x+1][y]>=0 ):    #UP/DOWN
+            moves.append([[x+1,y],[-1,0],map[x+1][y], i])
+            moves.append([[x-1,y],[1,0],map[x-1][y], i])
+
+        if( map[x][y-1]>=0 and map[x][y+1]>=0):    #LEFT/RIGHT
+            moves.append([[x,y+1],[0,-1],map[x][y+1], i])
+            moves.append([[x,y-1],[0,1],map[x][y-1], i])
+
+        if( map[x-1][y]==-9 and map[x+1][y]>=0 ):    #UP
+            moves.append([[x+1,y],[-1,0],map[x+1][y], i])
+
+        if( map[x-1][y]>=0 and map[x+1][y]==-9 ):    #DOWN
+            moves.append([[x-1,y],[1,0],map[x-1][y], i])
+
+        if( map[x][y-1]==-9 and map[x][y+1]>=0):    #LEFT
+            moves.append([[x,y+1],[0,-1],map[x][y+1], i])
+
+        if( map[x][y-1]>=0 and map[x][y+1]==-9):    #RIGHT
+            moves.append([[x,y-1],[0,1],map[x][y-1], i])
+
+        i=i+1
+
+    pass
+
+
 def Solve( state, goal):
 
+    # map : WALLS ONLY
     map = np.zeros((8,8),dtype='b')
 
     for val in state._wall:
         map[val[0]][val[1]]= -1
 
-    CountSteps( map, state)
+    Solve2( map, state, goal, 0, 0)
+
+
+def Solve2( map, state, goal, depth, total_steps):
+
+    # map2 : WALLS plus STEP COUNT
+    map2 = map.copy()
+
+    #Count steps to reachable blank squares
+    CountSteps( map2, state)
+
+    #print( map2)
+
+    if( depth> 2):
+        #exit()
+        return
+
+    #Remove illegible moves for the BOX
+    moves=[]  # list of [ targetPlayerPosition, moveDirection, steps, box no]
+    SetEligibleMoves( map2, state, moves)
+
+    #print( moves)
+
+    #print( state.get_hexdigest())
+
+    str_space=""
+
+    for i in range(0, depth+1):
+        str_space += "   "
+
+    #Try each possible move
+    for mov in moves:
+        steps = mov[2]
+        box_no = mov[3]
+        mov_dir = mov[1]
+
+        new_state = copy.deepcopy(state)
+
+        #print( new_state.get_hexdigest())
+
+        print( str_space, "Move Box:", box_no, "Steps:", steps, "Dir:", mov_dir)
+
+        new_state.moveBox( box_no, mov_dir)
+
+        #check if meet goal
+        if( new_state.matchGoal(goal)):
+            print( "Reach Goals!")
+            print( "Depth:", depth+1)
+            print( "Total Steps:", total_steps+steps+1)
+            exit()
+            return
+            #continue    #Find next alternative solution
+
+        #check if new_state is duplicate
+
+        #print( new_state.get_hexdigest())
+
+
+
+        #start a new node for search
+        Solve2( map, new_state, goal, depth+1, total_steps+steps+1)
+
+        continue
+
 
     pass 
 
@@ -188,6 +317,12 @@ s = STATE()
 mapstr = "#---WWWW-"+"#WWWW PW-"+"#W   B W-"+"#W B   WW"+"#WWB B  W"+"#-W  B  W"+"#-W  WWWW"+"#-WWWW---"
 
 goal = [[3,3],[3,4],[3,5],[4,4],[4,5]]
+
+goal = [[3,4],[3,2],[4,2],[4,4],[5,4]]  # one step
+
+goal = [[2,5],[3,2],[4,2],[4,4],[5,4]]  # one step
+
+goal = [[3,5],[3,2],[4,2],[4,4],[5,4]]  # two steps
 
 s.setup( mapstr)
 
