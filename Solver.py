d9906c549d055cd7f792cb6a10a5d564de10e8dc
timwuu <timwuu@gmail.com>
2020-04-08 15:59:35 +0800

    Improve Search approaches

--- Solver.py
+++ Solver.py
@@ -5,7 +5,10 @@ import datetime
 
 # Notation: [ROW][COL]
 # Note: Add Forbidden Cells to improve the efficiency
-#       Check duplicate state in the search tree, ?keep DEPTH info
+#       Check duplicate state in the search tree, keep DEPTH info
+#       Add Progress Monitoring
+#       ?Store Search Nodes for next batch
+#       ?Add Heuristic Move
 
 MAX_STEPS = 28  
 MAX_DEPTH = 6
@@ -13,8 +16,11 @@ MAP_ROW = 8
 MAP_COL = 8
 FORBIDDEN = [[1,4],[1,5],[2,1],[3,1],[4,6],[5,6],[7,2],[7,3]]
 
+g_para_total_state_searched = 0
 g_para_duplicate_state_count = 0
 g_para_duplicate_state_count2 = 0
+g_progress = 0.0
+g_progress_prv_time = datetime.datetime.now()
 
 def isNotForbidden( pos):
     return ( pos not in FORBIDDEN )
@@ -170,11 +176,17 @@ def CountSteps( map, state):
 
     pass
 
-def SetEligibleMoves( map, state, moves):
-    
-    i=0
+def SearchEligibleMoves( map, state, moves, log):
 
-    for elem in state._box:
+    i= -1
+    
+    # Try to move the same box first
+    if(len(log)):
+        i = log[-1][0]  # last moved box_no 
+        #lst_mov_dir = log[-1][2]
+
+        elem = state._box[i]
+
         x = elem[0]
         y = elem[1]
 
@@ -206,7 +218,46 @@ def SetEligibleMoves( map, state, moves):
             if( isNotForbidden([x,y+1])):
                 moves.append([[x,y-1],[0,1],map[x][y-1], i])
 
-        i=i+1
+    j=i
+
+    i= -1
+
+    for elem in state._box:
+        i+= 1
+
+        if(j==i):
+            continue
+
+        x = elem[0]
+        y = elem[1]
+
+        if( map[x-1][y]>=0 and map[x+1][y]>=0 ):    #UP/DOWN
+            if( isNotForbidden([x-1,y])):
+                moves.append([[x+1,y],[-1,0],map[x+1][y], i])
+            if( isNotForbidden([x+1,y])):
+                moves.append([[x-1,y],[1,0],map[x-1][y], i])
+
+        if( map[x][y-1]>=0 and map[x][y+1]>=0):    #LEFT/RIGHT
+            if( isNotForbidden([x,y-1])):
+                moves.append([[x,y+1],[0,-1],map[x][y+1], i])
+            if( isNotForbidden([x,y+1])):
+                moves.append([[x,y-1],[0,1],map[x][y-1], i])
+
+        if( map[x-1][y]==-9 and map[x+1][y]>=0 ):    #UP
+            if( isNotForbidden([x-1,y])):
+                moves.append([[x+1,y],[-1,0],map[x+1][y], i])
+
+        if( map[x-1][y]>=0 and map[x+1][y]==-9 ):    #DOWN
+            if( isNotForbidden([x+1,y])):
+                moves.append([[x-1,y],[1,0],map[x-1][y], i])
+
+        if( map[x][y-1]==-9 and map[x][y+1]>=0):    #LEFT
+            if( isNotForbidden([x,y-1])):
+                moves.append([[x,y+1],[0,-1],map[x][y+1], i])
+
+        if( map[x][y-1]>=0 and map[x][y+1]==-9):    #RIGHT
+            if( isNotForbidden([x,y+1])):
+                moves.append([[x,y-1],[0,1],map[x][y-1], i])
 
     pass
 
@@ -222,14 +273,18 @@ def Solve( state, goal):
     trace = {}
     log = []
 
-    if( not Solve2( map, state, goal, 0, 0, trace, log)):
+    if( not Solve2( map, state, goal, 0, 0, trace, log, 100.0)):
         print( "Cannot Solve!")
 
+    global g_para_total_state_searched
+    g_para_total_state_searched = len(trace)
+    
 
-def Solve2( map, state, goal, depth, total_steps, trace, log):
+
+def Solve2( map, state, goal, depth, total_steps, trace, log, progress_slot):
 
     if( total_steps> MAX_STEPS or depth> MAX_DEPTH):
-        #print( "total_steps:", total_steps, " depth:", depth)
+        output_progress( progress_slot)  # END_NODE
         return False
 
     # map2 : WALLS plus STEP COUNT
@@ -242,14 +297,22 @@ def Solve2( map, state, goal, depth, total_steps, trace, log):
 
     #Remove illegible moves for the BOX
     moves=[]  # list of [ targetPlayerPosition, moveDirection, steps, box no]
-    SetEligibleMoves( map2, state, moves)
+    SearchEligibleMoves( map2, state, moves, log)
 
-    #print( "DEPTH: {} MOVES: {} - {}".format( depth, len(moves), moves))
+    if( len(moves)):
+        mv_progress_slot = progress_slot/len(moves)
+    else:
+        output_progress( progress_slot)  # END_NODE
 
-    str_space = " Move Box: {:d} Steps: {:d} Dir: {}"
+    i_mov= -1
 
     #Try each possible move
     for mov in moves:
+
+        i_mov += 1
+
+        #if( depth<2): print( depth, mov, mv_progress_slot)
+        
         steps = mov[2]
         box_no = mov[3]
         mov_dir = mov[1]
@@ -257,8 +320,6 @@ def Solve2( map, state, goal, depth, total_steps, trace, log):
         new_state = copy.deepcopy(state)
 
         #print( new_state.get_hexdigest())
-
-        str_log = str_space.format( box_no, steps, mov_dir)
 
         #print( str_log)
 
@@ -270,10 +331,10 @@ def Solve2( map, state, goal, depth, total_steps, trace, log):
             print( "Depth:", depth+1)
             print( "Total Steps:", total_steps+steps+1)
     
-            log.append( str_log)
+            log.append([box_no, steps, mov_dir, i_mov])
 
-            for trc_log in log:
-                print( trc_log)
+            for l in log:
+                print( " Move Box: {:d} Steps: {:d} Dir: {} i: {}".format(l[0],l[1],l[2],l[3]))
             return True
 
         #check if new_state is duplicate
@@ -289,24 +350,38 @@ def Solve2( map, state, goal, depth, total_steps, trace, log):
 
             if( trace[key] < depth+1):
                 g_para_duplicate_state_count2 += 1
+                output_progress( mv_progress_slot)  # END_NODE
                 continue
 
-        log.append( str_log)
+        log.append([box_no, steps, mov_dir, i_mov])
         trace[key] = depth+1
 
         #print( new_state.get_hexdigest())
 
         #start a new node for search
-        if( Solve2( map, new_state, goal, depth+1, total_steps+steps+1, trace, log)):
+        if( Solve2( map, new_state, goal, depth+1, total_steps+steps+1, trace, log, mv_progress_slot)):
             return True
             #log.pop()
             #continue    #Find next alternative solution
         else:
-            log.pop()
+            log.pop()            
+            #output_progress( mv_progress_slot)
             #trace.pop(key)
         continue
 
     return False
+
+def output_progress( progress):
+    global g_progress
+    global g_progress_prv_time
+
+    g_progress += progress
+    
+    tmp = datetime.datetime.now()
+
+    if( tmp - g_progress_prv_time > datetime.timedelta(seconds=2.0)):
+        print( "progress: {:.4f}%".format(g_progress))
+        g_progress_prv_time = tmp
 
 s = STATE()
 
@@ -330,15 +405,35 @@ goal = [[3,4],[3,3],[4,2],[4,3],[5,5]]  # two steps
 
 goal = [[3,4],[3,3],[3,2],[4,3],[5,5]]  # two steps
 
+# Time Used:0:00:01.915810
 MAX_STEPS = 28
 MAX_DEPTH = 6
 goal = [[3,4],[3,3],[2,2],[4,3],[5,5]]
 
+# Time Used:0:01:04.020900
 MAX_STEPS = 32
 MAX_DEPTH = 9
 goal = [[3,4],[3,3],[2,5],[4,3],[5,5]]
 
+# # Time Used:0:03:30.385308
+# MAX_STEPS = 33
+# MAX_DEPTH = 10
+# goal = [[4,4],[3,3],[2,5],[4,3],[5,5]]
+
+# Time Used:0:58:33.046980
+MAX_STEPS = 40
+MAX_DEPTH = 13
+goal = [[4,4],[3,3],[2,5],[4,3],[5,2]]
+
+# MAX_DEPTH = 16
+# goal = [[4,4],[3,3],[2,5],[4,3],[2,2]]
+
+# MAX_DEPTH = 17
+# goal = [[4,4],[3,4],[2,5],[4,3],[2,2]]
+
 s.setup( mapstr)
+
+g_progress_prv_time = datetime.datetime.now()
 
 start_time = datetime.datetime.now()
 
@@ -346,8 +441,30 @@ Solve( s, goal)
 
 diff_time = datetime.datetime.now() - start_time
 
-print( "Time Diff:{}".format(diff_time))
+print( "Time Used:{}".format(diff_time))
+print( "Total State Searched:{}".format(g_para_total_state_searched))
 print( "Duplicate Key Count :{}".format(g_para_duplicate_state_count))
 print( "Duplicate Key Count2:{}".format(g_para_duplicate_state_count2))
 # Setup Map and State:{ Goal, Box, Player, Wall }
 
+# Logs:
+# Time Used:0:58:33.046980
+# MAX_STEPS = 40
+# MAX_DEPTH = 13
+# goal = [[4,4],[3,3],[2,5],[4,3],[5,2]]
+
+# Depth: 13
+# Total Steps: 40
+#  Move Box: 0 Steps: 1 Dir: [1, 0]
+#  Move Box: 4 Steps: 4 Dir: [0, 1]
+#  Move Box: 1 Steps: 7 Dir: [0, 1]
+#  Move Box: 3 Steps: 6 Dir: [0, -1]
+#  Move Box: 2 Steps: 3 Dir: [-1, 0]
+#  Move Box: 2 Steps: 0 Dir: [-1, 0]
+#  Move Box: 2 Steps: 2 Dir: [0, 1]
+#  Move Box: 2 Steps: 0 Dir: [0, 1]
+#  Move Box: 2 Steps: 0 Dir: [0, 1]
+#  Move Box: 0 Steps: 0 Dir: [1, 0]
+#  Move Box: 4 Steps: 4 Dir: [0, -1]
+#  Move Box: 4 Steps: 0 Dir: [0, -1]
+#  Move Box: 4 Steps: 0 Dir: [0, -1]
